37b66436c3be5b99bd7b65e4c48cc9c913286d80
re5pawn <s4ichulin@gmail.com>
2016-04-08 12:12:14 +0300

    add clear-field button

--- js/components/SearchBar.js
+++ js/components/SearchBar.js
@@ -10,12 +10,30 @@ class SearchBar extends Component {
 			this.props.onChange && this.props.onChange(query);
 		}
 
+		cleanField() {
+			this.refs.searchField.value = '';
+			this.props.onChange && this.props.onChange('');
+		}
+
 		render() {
+			let fieldCleanerStyles = {
+				position: 'absolute',
+				top: 0,
+				right: 0,
+				padding: '8px 16px',
+				color: '#2B3E50'
+			};
+
 			return (
 				<div className="row">
 					<div className="col-md-12">
-						<input type="text" className="form-control" placeholder="Search by name..."
-						onChange={this.handleChange.bind(this)} />	
+						<div style={{position: 'relative'}}>
+							<input type="text" className="form-control" placeholder="Search by name..."
+							ref="searchField" onChange={this.handleChange.bind(this)} />
+							<button type="button" className="close" style={fieldCleanerStyles} onClick={this.cleanField.bind(this)}>
+								&times;
+							</button>
+						</div>
 					</div>
 				</div>
 				
