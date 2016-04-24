685d4c4e8dcc7e0df2cae4956044aa7be8c68185
re5pawn <s4ichulin@gmail.com>
2016-04-24 12:39:21 +0300

    refactored class to function

--- js/components/ActiveUser.js
+++ js/components/ActiveUser.js
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
 import * as Utils from '../utils';
 
-class ActiveUser extends Component {
-		constructor(props) {
-			super(props);
-		}
+const ActiveUser = ({user}) => {
+		let optClassName = Object.keys(user).length ? '' : 'hide';
 
-		render() {
-			let user = this.props.user;
-			let optClassName = Object.keys(user).length ? '' : 'hide';
-
-			return (
-				<div className={Utils.concatClassNames(optClassName, 'panel', 'panel-default')}>
-					<div className="panel-heading text-center">{user.name}</div>
-					<div className="panel-body">
-						<div className="text-center">
-							<img src={Utils.getUserImage(user.image)} alt="User Image" className="active-user-image" />
-						</div>
-						<ul className="list-group">
-							<li className="list-group-item"><b>Age:</b> {user.age}</li>
-							<li className="list-group-item"><b>Favorite animal:</b> {user.image}</li>
-							<li className="list-group-item"><b>Phone:</b> {user.phone}</li><hr/>
-							<li className="list-group-item"><b>Favorite phrase:</b> {user.phrase}</li>
-						</ul>
+		return (
+			<div className={Utils.concatClassNames(optClassName, 'panel', 'panel-default')}>
+				<div className="panel-heading text-center">{user.name}</div>
+				<div className="panel-body">
+					<div className="text-center">
+						<img src={Utils.getUserImage(user.image)} alt="User Image" className="active-user-image" />
 					</div>
+					<ul className="list-group">
+						<li className="list-group-item"><b>Age:</b> {user.age}</li>
+						<li className="list-group-item"><b>Favorite animal:</b> {user.image}</li>
+						<li className="list-group-item"><b>Phone:</b> {user.phone}</li><hr/>
+						<li className="list-group-item"><b>Favorite phrase:</b> {user.phrase}</li>
+					</ul>
 				</div>
-			);
-		}
+			</div>
+		);
 }
 
 export default ActiveUser;
