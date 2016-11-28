18d8e5b8e2c9e43589ede7e0d7deb6382759e205
serchulin <sergey@ukr-soft.com>
2016-11-28 13:09:55 +0200

    add redux devtools

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import { appReducer, handleLoadedDataMiddleware } from './appReducer';
 
-const store = createStore(appReducer, applyMiddleware(handleLoadedDataMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(appReducer, composeEnhancers(applyMiddleware(handleLoadedDataMiddleware)));
 
 ReactDOM.render(
   <Provider store={store}>
