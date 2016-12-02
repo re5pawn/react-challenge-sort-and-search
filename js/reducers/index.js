83c8662fec361cb80a646db0200fb62a6f5972fa
re5pawn <s4ichulin@gmail.com>
2016-12-02 18:48:43 +0200

    add combineReducers implementation

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,8 +1,17 @@
-import { combineReducers } from 'redux';
+//import { combineReducers } from 'redux';
 
 import { activeUserId } from './activeUserId';
 import { searchQuery } from './searchQuery';
 import { users } from './users';
+
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
+  }
+};
 
 export default combineReducers({
   users,
