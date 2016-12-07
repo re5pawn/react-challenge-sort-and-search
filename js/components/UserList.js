749a2c0fa1b7e10fccdb154429ccbb3e5d94f672
re5pawn <s4ichulin@gmail.com>
2016-12-07 20:32:29 +0200

    add position sticky for ActiveUser

--- js/components/UserList.js
+++ js/components/UserList.js
@@ -14,7 +14,8 @@ class UserList extends Component {
 		render() {
 			return (
 				<div className="row">
-					<div className="col-md-3">
+					<div className="col-md-3"
+               style={{position: '-webkit-sticky', position: 'sticky', top: '15px'}}>
 						<ActiveUser user={this.props.activeUser} />
 					</div>
 
